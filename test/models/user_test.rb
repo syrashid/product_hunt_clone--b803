9d17f376e42cf3e4c0a1ad4312e0280266521352
Sy Rashid <sy.rashid.11@gmail.com>
2022-03-04 10:39:25 +0000

    TEST: Wrote tests and now continuous integration workflow

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
-require "test_helper"
-
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    # Setup
+    user = User.new(first_name: "john", last_name: "lennon")
+
+    # Execute
+    full_name = user.full_name
+
+    # Verify
+    assert_equal "John Lennon", full_name
+
+    # Teardown
+  end
 end
